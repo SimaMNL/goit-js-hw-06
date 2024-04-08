b8983e5c84ec16d712d38459c9fe532ed4c74fe8
SimaMNL <144942961+SimaMNL@users.noreply.github.com>
2024-04-08 21:33:10 +0200

    da

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formElements = loginForm.elements;
+
+  const formData = {};
+
+  for (let element of formElements) {
+    if (element.type !== "submit") {
+      if (element.value.trim() === "") {
+        alert("Toate câmpurile trebuie completate!");
+        return;
+      }
+      formData[element.name] = element.value;
+    }
+  }
+
+  console.log(formData);
+
+  loginForm.reset();
+});
